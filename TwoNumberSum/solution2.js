13cfef8ea492958c8f63fe692927fcf20a453095
Eddie Cuellar <heriberto.cuellar1329@gmail.com>
2021-02-02 23:57:50 -0600

    second solution for twoNumberSum

---
+++ TwoNumberSum/solution2.js
@@ -0,0 +1,22 @@
+const { performance } = require('perf_hooks');
+
+let array = [3, 5, -4, 8, 11, 1, -1, 6];
+let targetSum = 10;
+
+function twoNumberSum(array, targetSum) {
+    const nums = {};
+    for (const num of array) {
+        const potentialMatch = targetSum - num;
+        if (potentialMatch in nums) {
+            return [potentialMatch, num];
+        } else {
+            nums[num] = true;
+        }
+    }
+    return [];
+}
+
+let t0 = performance.now();
+console.log(twoNumberSum(array, targetSum));
+let t1 = performance.now();
+console.log("time to execute = " + (t1 - t0) + "milliseconds.");
